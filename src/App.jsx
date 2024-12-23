5c25b6a63ea3ddc6875e8667cc5201a45db17ec9
Akshay Patil <akshaypatil22082002@gmail.com>
2024-12-23 19:23:23 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import DashBoard from './pages/DashBoard'
+import "./App.css";
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+
+export default function App() {
+  return (
+    <>
+        <DashBoard />
+    </>
+  )
+}
---
+++ src/pages/DashBoard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import SliderNav from '../parts/SliderNav'
+import NavigationBar from '../parts/NavigationBar'
+
+export default function DashBoard() {
+  return (
+   <>
+   <NavigationBar />
+   <SliderNav />
+   </>
+  )
+}
